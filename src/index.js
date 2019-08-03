aca0e565b16ec7dd786e52760772c246c40945b9
pi3rrot <4dakto@gmail.com>
2019-08-03 14:50:08 +0900

    init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { ApolloProvider } from "react-apollo";
+import client from "./apollo";
+import GlobalStyles from "./globalStyles";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <GlobalStyles />
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
